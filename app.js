81632e31373c23a163b01c798e4f5931797a5cb5
Riddhiman <shubro17@outlook.com>
2022-09-10 20:55:17 +0530

    Added routes to app

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const router = require('./routes/user.router')
 const path = require('path')
-const router = require('./routes/user.router')
 const uri = "mongodb+srv://Riddhiman:123@cluster0.gqyxkh7.mongodb.net/MLH_HACKODISHA?retryWrites=true&w=majority"
 mongoose.connect(uri)
 const app = express()
