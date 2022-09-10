3c2f8ac890b8f769e4d6ba72904f5ac1d32d3c27
Jahnavi Darbhamulla <76477365+JahnaviDarbhamulla@users.noreply.github.com>
2022-09-11 04:12:35 +0530

    Upload Model Files

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const Tesseract = require("tesseract.js");
+
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null,'./uploads/');
+  },
+  filename:(req,file,cb)=>{
+    cb(null,file.originalname)
+  }
+})
+
+const upload =  multer({storage:storage})
+
+app.post('/api/upload', upload.single('UploadedImage'),(req,res)=>{
+  console.log(req.file);
+
+  try{
+    Tesseract.recognize(
+      'uploads/'+req.file.filename,
+      'eng',
+      {logger:m =>console.log(m)}
+    ).then(({data:{text}})=>{
+
+      return res.json(
+        {
+          message:text
+        }
+      )
+    })
+  }catch(error){
+    console.error(error)
+  }
+
+})
+
+app.listen(4000, ()=>{
+  console.log('server is up and running on port 3000')
+})
